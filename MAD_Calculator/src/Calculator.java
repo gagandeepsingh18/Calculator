0c344853f3961acba65f063007ef2c044605dc36
1992606 <1992606@E20735.CEGEPGIM.LCL>
2019-07-11 15:27:00 -0400

    Gagan - Start coding

--- MAD_Calculator/src/Calculator.java
+++ MAD_Calculator/src/Calculator.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Calculator {
 
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int number1;
+		int number2;
 	}
 
 }
