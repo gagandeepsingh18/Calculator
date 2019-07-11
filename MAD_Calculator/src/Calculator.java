e8a9a65fab7ed8164cf1b26c74897dee8fc63669
1992606 <1992606@E20735.CEGEPGIM.LCL>
2019-07-11 16:02:02 -0400

    asd

--- MAD_Calculator/src/Calculator.java
+++ MAD_Calculator/src/Calculator.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import java.lang.Math;
 
 public class Calculator {
 
@@ -8,16 +9,23 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number1=0;
-		int number2=0;
-		int operator;
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter number 1");
-		number1=obj.nextInt();
-		System.out.println("Enter number 1");
-		number2=obj.nextInt();
-		System.out.println("Enter operator");
-		operator=obj.nextInt();
+		int number1;
+		int number2;
+		System.out.println("CHANGES");
+		switch (operator) {
+		case 1: 
+			System.out.println(number1 + number2);
+		case 2: 
+			System.out.println(number1 - number2);
+		case 3: 
+			System.out.println(number1 * number2);
+		case 4: 
+			System.out.println(number1 / number2);
+		case 5: 
+			System.out.println(number1 % number2);
+		case 6: 
+			System.out.println(Math.pow(number1, number2);	
+		}
 	}
 
 }
